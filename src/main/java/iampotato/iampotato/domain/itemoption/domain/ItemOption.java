9eea6eddf19329245252ad709f34c492516b09b8
Jinhoon Park <g22203@naver.com>
2023-03-21 23:13:56 +0900

    feat : add ItemOptionStatus domain variable (#12)

--- src/main/java/iampotato/iampotato/domain/itemoption/domain/ItemOption.java
+++ src/main/java/iampotato/iampotato/domain/itemoption/domain/ItemOption.java
@@ -1,4 +1,33 @@
 package iampotato.iampotato.domain.itemoption.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemOption {
+
+    @Id @GeneratedValue
+    @Column(name = "item_option_id")
+    private Long id;
+
+    private String category; // 사이즈, 토핑추가, 맵기정도 등 카테고리 구분하기 위함
+
+    private String name;
+
+    private int price;
+
+    private String description;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    @Enumerated(EnumType.STRING)
+    private ItemOptionStatus status;
 }
---
+++ src/main/java/iampotato/iampotato/domain/itemoption/domain/ItemOptionStatus.java
@@ -0,0 +1,9 @@
+package iampotato.iampotato.domain.itemoption.domain;
+
+/**
+ * FOR_SALE : 판매중
+ * SOLD_OUT : 품절
+ */
+public enum ItemOptionStatus {
+    FOR_SALE, SOLD_OUT
+}

3a235ff1ae685220ead9761d83e55ac4a802ad44
Jinhoon Park <g22203@naver.com>
2023-03-21 22:28:33 +0900

    feat : add order domain variable (#12)

--- src/main/java/iampotato/iampotato/domain/order/domain/Order.java
+++ src/main/java/iampotato/iampotato/domain/order/domain/Order.java
@@ -1,4 +1,44 @@
 package iampotato.iampotato.domain.order.domain;
 
+import iampotato.iampotato.domain.orderitem.domain.OrderItem;
+import iampotato.iampotato.domain.review.domain.Review;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @OneToMany
+    @JoinColumn(name = "order_item_id")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    private int totalPrice;
+
+    private int totalPeople;
+
+    private String qrValue;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; // 주문 상태 [ORDER, WAITQR, FINISH]
 }

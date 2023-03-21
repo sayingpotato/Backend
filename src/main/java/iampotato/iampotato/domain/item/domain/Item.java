fd0542aaa096e44a414bd682a4618167cee4b02f
Jinhoon Park <g22203@naver.com>
2023-03-21 23:06:09 +0900

    feat : add item domain variable (#12)

--- src/main/java/iampotato/iampotato/domain/item/domain/Item.java
+++ src/main/java/iampotato/iampotato/domain/item/domain/Item.java
@@ -1,4 +1,48 @@
 package iampotato.iampotato.domain.item.domain;
 
+import iampotato.iampotato.domain.itemoption.domain.ItemOption;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Item -> ItemOption = 1 : N
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    @OneToMany
+    private List<ItemOption> itemOption = new ArrayList<>();
+
+    private String category;
+
+    private String name;
+
+    private int price;
+
+    private String img;
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ItemPopularity popularity; // [NONE, MOST_FAVORITE, MANY_REORDER]
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    @Enumerated(EnumType.STRING)
+    private ItemStatus status; // [FOR_SALE, SOLD_OUT]
+
 }
---
+++ src/main/java/iampotato/iampotato/domain/item/domain/ItemPopularity.java
@@ -0,0 +1,10 @@
+package iampotato.iampotato.domain.item.domain;
+
+/**
+ * NONE : 없음, 기본값
+ * MOST_FAVORITE : 인기 많음
+ * MANY_REORDER : 재주문 많음
+ */
+public enum ItemPopularity {
+    NONE, MOST_FAVORITE, MANY_REORDER
+}

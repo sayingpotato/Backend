32c983c7e967846e6b8816fe5a6905d1e9c48a4a
Jinhoon Park <g22203@naver.com>
2023-03-21 22:54:41 +0900

    feat : add orderItem domain variable (#12)

--- src/main/java/iampotato/iampotato/domain/orderitem/domain/OrderItem.java
+++ src/main/java/iampotato/iampotato/domain/orderitem/domain/OrderItem.java
@@ -1,4 +1,29 @@
 package iampotato.iampotato.domain.orderitem.domain;
 
+import iampotato.iampotato.domain.item.domain.Item;
+import iampotato.iampotato.domain.itemoption.domain.ItemOption;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
+
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_option_id")
+    private ItemOption itemOption;
+
+    private int totalPrice; // item 가격 + itemOption 가격
 }

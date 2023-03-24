8e8c4ea82cc676d680940bd3ba2b1e4216af259a
Jinhoon Park <g22203@naver.com>
2023-03-24 21:46:52 +0900

    feat : add discount domain variable (#12)

--- src/main/java/iampotato/iampotato/domain/discount/domain/Discount.java
+++ src/main/java/iampotato/iampotato/domain/discount/domain/Discount.java
@@ -1,4 +1,31 @@
 package iampotato.iampotato.domain.discount.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Discount {
+
+    @Id @GeneratedValue
+    @Column(name = "discount_id")
+    private Long id;
+
+    private int people;
+
+    private int discountRatio;
+
+    private String day; // 할인하는 요일
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
 }

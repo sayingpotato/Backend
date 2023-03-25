a18e5526d633a9bae6798f67465a09e10c075269
L-o-g-a-n <xy.logandev@gmail.com>
2023-03-25 23:23:34 +0900

    feat : add a SideOwner domain variable (#13)

--- src/main/java/iampotato/iampotato/domain/sideowner/domain/SideOwner.java
+++ src/main/java/iampotato/iampotato/domain/sideowner/domain/SideOwner.java
@@ -1,4 +1,30 @@
 package iampotato.iampotato.domain.sideowner.domain;
 
+import iampotato.iampotato.domain.owner.domain.Owner;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
 public class SideOwner {
+
+    @Id @GeneratedValue
+    @Column(name = "side_owner_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    private String password;
+
+    private String nickname;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    private String grade;
 }

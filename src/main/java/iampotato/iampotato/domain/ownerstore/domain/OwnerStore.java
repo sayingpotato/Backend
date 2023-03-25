8ebb440b5cae9096a668bcbda6917d922329e9bc
L-o-g-a-n <xy.logandev@gmail.com>
2023-03-26 00:20:14 +0900

    feat : add an OwnerStore domain variable (#13)

--- src/main/java/iampotato/iampotato/domain/ownerstore/domain/OwnerStore.java
+++ src/main/java/iampotato/iampotato/domain/ownerstore/domain/OwnerStore.java
@@ -1,4 +1,25 @@
 package iampotato.iampotato.domain.ownerstore.domain;
 
+import iampotato.iampotato.domain.owner.domain.Owner;
+import iampotato.iampotato.domain.store.domain.Store;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class OwnerStore {
+
+    @Id @GeneratedValue
+    @Column(name = "owner_store_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id")
+    private Owner owner;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
 }

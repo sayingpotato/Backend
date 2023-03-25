e8768f452d5bde0339e215a3bda659f2bdac64ee
L-o-g-a-n <xy.logandev@gmail.com>
2023-03-25 22:40:05 +0900

    feat : add a OwnerStatus enum type (#13)

--- src/main/java/iampotato/iampotato/domain/owner/domain/Owner.java
+++ src/main/java/iampotato/iampotato/domain/owner/domain/Owner.java
@@ -1,4 +1,27 @@
 package iampotato.iampotato.domain.owner.domain;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Owner {
+
+    @Id @GeneratedValue
+    @Column(name = "owner_id")
+    private Long id;
+
+    private String loginId;
+
+    private String password;
+
+    private String ssn;
+
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private OwnerStatus ownerStatus;
+
+
 }

0e2a29ec9f0271cb44562c3dc4d75e8561fa35af
Jinhoon Park <g22203@naver.com>
2023-03-27 23:11:57 +0900

    feat : add ItemOptionCategory (#12)

--- src/main/java/iampotato/iampotato/domain/itemoption/domain/ItemOption.java
+++ src/main/java/iampotato/iampotato/domain/itemoption/domain/ItemOption.java
@@ -24,7 +24,7 @@ public class ItemOption {
     @Column(name = "item_id")
     private Item item;
 
-    private String category; // 사이즈, 토핑추가, 맵기정도 등 카테고리 구분하기 위함
+    private ItemOptionCategory category; // 사이즈, 토핑추가, 맵기정도 등 카테고리 구분하기 위함
 
     private String name;
 

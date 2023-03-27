c3822f50ec8b7135e4127e676abf60cd833bc824
Jinhoon Park <g22203@naver.com>
2023-03-27 23:13:33 +0900

    feat : modify OrderItem relation (#12)

--- src/main/java/iampotato/iampotato/domain/orderitem/domain/OrderItem.java
+++ src/main/java/iampotato/iampotato/domain/orderitem/domain/OrderItem.java
@@ -2,6 +2,7 @@ package iampotato.iampotato.domain.orderitem.domain;
 
 import iampotato.iampotato.domain.item.domain.Item;
 import iampotato.iampotato.domain.itemoption.domain.ItemOption;
+import iampotato.iampotato.domain.order.domain.Order;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 /**
+ * OrderItem <-> Order = N : 1
  * OrderItem -> Item = N : 1
  * OrderItem -> ItemOption = N : 1
  */
@@ -20,6 +22,10 @@ public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")

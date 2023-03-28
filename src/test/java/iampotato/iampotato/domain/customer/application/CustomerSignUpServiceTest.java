544ecf22193f1009a8dec402e6e2c0ba0e3e61af
L-o-g-a-n <xy.logandev@gmail.com>
2023-03-28 17:04:38 +0900

    feat : add some test codes about CustomerSignUpService (#16)

--- src/test/java/iampotato/iampotato/domain/customer/application/CustomerSignUpServiceTest.java
+++ src/test/java/iampotato/iampotato/domain/customer/application/CustomerSignUpServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,7 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)    //spring이랑 완전히 integration해서 테스트 하기 위해
 @SpringBootTest //spring이랑 완전히 integration해서 테스트 하기 위함
 @Transactional  //테스트 메소드가 끝난 뒤 롤백을 하기 위해
+@Rollback(false)
 //정확히는 EntityManager가 flush를 하지 못하게 하여 DB에 커밋이 안되는 것
 public class CustomerSignUpServiceTest {
 
@@ -25,8 +27,14 @@ public class CustomerSignUpServiceTest {
 
     @Test
     public void 회원가입() throws Exception {
+        //given
         Customer customer = Customer.createCustomer("test1", "1q2w3e4r1!", "로건");
 
+        //when
+        Long saveId = customerSignUpService.signUp(customer);
+
+        //then
+        assertEquals(customer, customerRepository.findOne(saveId));
     }
 
 

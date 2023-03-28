b20799f9a64a0efe60b2f7be3ba4a8af5f7ead6c
Jinhoon Park <g22203@naver.com>
2023-03-28 22:08:27 +0900

    feat : add StoreDTO (#19)

---
+++ src/main/java/iampotato/iampotato/domain/store/dto/StoreRegistrationResponse.java
@@ -0,0 +1,12 @@
+package iampotato.iampotato.domain.store.dto;
+
+import iampotato.iampotato.domain.store.domain.*;
+
+public class StoreRegistrationResponse {
+
+    Long id;
+
+    public StoreRegistrationResponse(Long id) {
+        this.id = id;
+    }
+}

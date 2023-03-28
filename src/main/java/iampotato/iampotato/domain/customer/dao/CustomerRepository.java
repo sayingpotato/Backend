65c3206741587e890aa4a7708e0a78b647d9dea1
L-o-g-a-n <xy.logandev@gmail.com>
2023-03-28 20:17:50 +0900

    feat : add a findByNickname (#21)

--- src/main/java/iampotato/iampotato/domain/customer/dao/CustomerRepository.java
+++ src/main/java/iampotato/iampotato/domain/customer/dao/CustomerRepository.java
@@ -26,10 +26,18 @@ public class CustomerRepository {
                 .getResultList();
     }
 
-    public List<Customer> findByLoginId(String loginId) {
+    public List<Customer> findByLoginId(String loginId) {   //LoginId를 통해 모든 Customer 조회
         return em.createQuery("select c from Customer c where c.loginId = :loginId", Customer.class)    //:loginId라고 하여 밑에서 setParameter를 통해 "loginId"의 value와 바인딩
                 .setParameter("loginId", loginId)   //위에 있는 :loginId가 여기 파리미터의 Key값과 바인딩 되어서 value가 위로 넘어가게 됌
                 .getResultList();
     }
+
+    public List<Customer> findByNickname(String nickname) { //Nickname을 통해 모든 Customer 조회
+        return em.createQuery("select c from Customer c where c.nickname = :nickname", Customer.class)
+                .setParameter("nickname", nickname)
+                .getResultList();
+    }
+
+
 
 }

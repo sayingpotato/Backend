48579acdc2479966cd393d8f45dd39f824b58998
L-o-g-a-n <xy.logandev@gmail.com>
2023-04-07 18:09:58 +0900

    feat: change factory method to builder pattern (#35)

--- src/main/java/iampotato/iampotato/domain/customer/api/CustomerApi.java
+++ src/main/java/iampotato/iampotato/domain/customer/api/CustomerApi.java
@@ -19,7 +19,11 @@ public class CustomerApi {
     @PostMapping("/api/v1/customers")
     public SignUpResponse signUp(@RequestBody SignUpRequest signUpRequest) {    //회원 가입하는 POST API
         //Spring security로 Password Hash 암호화 로직 추가하기
-        Customer customer = Customer.createCustomer(signUpRequest.getLoginId(), signUpRequest.getPassword(), signUpRequest.getNickname());
+        Customer customer = Customer.builder()
+                .loginId(signUpRequest.getLoginId())
+                .password(signUpRequest.getPassword())
+                .nickname(signUpRequest.getNickname())
+                .build();
         Long id = customerSignUpService.signUp(customer);
         return new SignUpResponse(id);
     }
--- src/main/java/iampotato/iampotato/domain/customer/domain/Customer.java
+++ src/main/java/iampotato/iampotato/domain/customer/domain/Customer.java
@@ -1,27 +1,28 @@
 package iampotato.iampotato.domain.customer.domain;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Builder    //==정적 팩토리 메서드에서 빌더 패턴으로 변경!==//
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Customer {
 
     //==정적 팩토리 메서드==//
-    public static Customer createCustomer(String loginId, String password, String nickname) {
-        Customer customer = new Customer();
-        customer.loginId = loginId;
-        customer.password = password;
-        customer.nickname = nickname;
-        customer.createdDate = LocalDateTime.now();
-        customer.modifiedDate = LocalDateTime.now();
-        return customer;
-    }
+//    public static Customer createCustomer(String loginId, String password, String nickname) {
+//        Customer customer = new Customer();
+//        customer.loginId = loginId;
+//        customer.password = password;
+//        customer.nickname = nickname;
+//        customer.createdDate = LocalDateTime.now();
+//        customer.modifiedDate = LocalDateTime.now();
+//        return customer;
+//    }
+
     @Id @GeneratedValue
     @Column(name = "customer_id")
     private Long id;

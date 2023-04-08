2306a3a221e1d994003aaeb805082398acac64a5
xylogan <xy.logandev@gmail.com>
2023-04-08 17:42:02 +0900

    feat : add an uploadImage api (#35)

--- src/main/java/iampotato/iampotato/domain/customer/api/CustomerApi.java
+++ src/main/java/iampotato/iampotato/domain/customer/api/CustomerApi.java
@@ -1,20 +1,23 @@
 package iampotato.iampotato.domain.customer.api;
 
+import iampotato.iampotato.domain.customer.application.CustomerImageService;
 import iampotato.iampotato.domain.customer.application.CustomerSignUpService;
 import iampotato.iampotato.domain.customer.domain.Customer;
 import iampotato.iampotato.domain.customer.dto.SignUpRequest;
 import iampotato.iampotato.domain.customer.dto.SignUpResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
 public class CustomerApi {
 
     private final CustomerSignUpService customerSignUpService;
+    private final CustomerImageService customerImageService;
 
     @PostMapping("/api/v1/customers")
     public SignUpResponse signUp(@RequestBody SignUpRequest signUpRequest) {    //회원 가입하는 POST API
@@ -26,5 +29,11 @@ public class CustomerApi {
                 .build();
         Long id = customerSignUpService.signUp(customer);
         return new SignUpResponse(id);
+    }
+
+    @PutMapping("/api/v1/customers/{id}/image")
+    public UploadImageResponse uploadImage(@PathVariable("id") Long customerId, MultipartFile multipartFile) throws Exception {
+        Customer customer = customerImageService.uploadImage(customerId, multipartFile);
+        return new UploadImageResponse(customerId, customer.getCustomerImage());
     }
 }

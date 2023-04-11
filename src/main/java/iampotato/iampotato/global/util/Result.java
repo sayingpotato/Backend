d42249f02b670d3b25391a3a50eee5c9252af21e
Jinhoon Park <g22203@naver.com>
2023-04-11 19:51:17 +0900

    feat : add Result global util (#28)

---
+++ src/main/java/iampotato/iampotato/global/util/Result.java
@@ -0,0 +1,25 @@
+package iampotato.iampotato.global.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 해당 유틸은 API 사용시 결과를 반환할때
+ * Result 클래스로 한번 더 감싸주기 위해서 입니다.
+ */
+@Data
+@AllArgsConstructor
+public class Result<T> {
+
+    public static final String MESSAGE_OK = "ok";
+
+    public static final int CODE_CONTINUE = 100;
+    public static final int CODE_SUCCESS = 200;
+    public static final int CODE_REDIRECT = 300;
+    public static final int CODE_CLIENT_ERROR = 400;
+    public static final int CODE_SERVER_ERROR = 500;
+
+    private int code;
+    private String message;
+    private T data;
+}

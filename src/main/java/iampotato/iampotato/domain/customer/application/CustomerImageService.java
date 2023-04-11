6cf4a00bc61aa682cdf0fb3465dad141e5c98e85
xylogan <xy.logandev@gmail.com>
2023-04-11 23:31:53 +0900

    fix : fix an error about before static method (#35)

--- src/main/java/iampotato/iampotato/domain/customer/application/CustomerImageService.java
+++ src/main/java/iampotato/iampotato/domain/customer/application/CustomerImageService.java
@@ -18,9 +18,8 @@ public class CustomerImageService {
     @Transactional  //CustomerImage(학생증 이미지)를 업로드해주는 서비스 로직이다.
     public Customer uploadImage(Long customerId, MultipartFile multipartFile) throws Exception {
         //parseImageInfo() 과정에서 Exception이 발생할 수 있는데 이러면 상단 호출 메소드(컨트롤러)로 예외를 던진다.
-        CustomerImage parseCustomerImage = Customer.parseImageInfo(multipartFile);
         Customer findCustomer = customerRepository.findOne(customerId);
-
+        CustomerImage parseCustomerImage = findCustomer.parseImageInfo(multipartFile);
         findCustomer.updateCustomerImage(parseCustomerImage);   //customer 엔티티 내 customerImage 필드를 업데이트해주는 비즈니스 로직을 만들었다.
         //이처럼 Transaction 단위 내 JPA 영속성 컨텍스트에서 관리하는 엔티티 내 필드 상태에 변화가 생기게 되면 Dirty checking을 통해 알아서 업데이트 쿼리를 날려주게 된다.
 

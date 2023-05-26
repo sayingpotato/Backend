36de600ff632cdf321ad76fd3faa8af420e6d260
Jinhoon Park <g22203@naver.com>
2023-05-26 21:54:52 +0900

    feat : modify store using first collection (#46)

--- src/main/java/iampotato/iampotato/domain/store/domain/Store.java
+++ src/main/java/iampotato/iampotato/domain/store/domain/Store.java
@@ -1,22 +1,24 @@
 package iampotato.iampotato.domain.store.domain;
 
-import iampotato.iampotato.domain.discount.domain.Discount;
-import iampotato.iampotato.domain.item.domain.Item;
-import iampotato.iampotato.domain.review.domain.Review;
+import iampotato.iampotato.domain.discount.domain.Discounts;
+import iampotato.iampotato.domain.item.domain.Items;
+import iampotato.iampotato.domain.review.domain.Reviews;
+import iampotato.iampotato.domain.storeimage.domain.StoreImages;
+import iampotato.iampotato.domain.storeoperationhour.domain.StoreOperationHours;
 import lombok.*;
-import org.hibernate.annotations.BatchSize;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 import org.locationtech.jts.geom.Point;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.*;
+
 
 /**
  * Store <-> Item = 1 : N
  * Store <-> Discount = 1 : N
  * Store <-> Review = 1 : N
+ * Store <-> StoreImage = 1 : N
+ * Store <-> StoreOperationHour = 1 : N
  */
 @Entity
 @Getter
@@ -26,22 +28,19 @@ import java.util.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Store {
 
-    private static final String DEFAULT_STORE_URL = "'aa'";
-
     @Id @GeneratedValue
     @Column(name = "store_id")
     private Long id;
 
     // Store 을 삭제하면 해당 가게의 상품과 상품옵션도 같이 지워지도록 CascadeType.ALL 로 설정
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
-    private List<Item> items = new ArrayList<>();
+    @Embedded
+    private Items items;
 
-    @BatchSize(size = 100)
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
-    private List<Discount> discounts = new ArrayList<>();
+    @Embedded
+    private Discounts discounts;
 
-    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
-    private List<Review> reviews = new ArrayList<>();
+    @Embedded
+    private Reviews reviews;
 
     @Enumerated(EnumType.STRING)
     private StoreCategory category;
@@ -60,22 +59,26 @@ public class Store {
     @Embedded
     private StoreMapThumbnail storeMapThumbnail;
 
+    private String storeTodayDiscountThumbnail;
+
     @Embedded
     private StoreTopReview storeTopReview;
 
     @Embedded
     private StoreTopItem storeTopItem;
 
-    @ColumnDefault(DEFAULT_STORE_URL)
-    private String storeImg;
+    @Embedded
+    private StoreImages storeImages;
 
     private String description;
 
     private String tableImg;
 
-    private String operationHour;
+    @Embedded
+    private StoreOperationHours storeOperationHours;
 
-    private String closedDay;
+    @Enumerated(EnumType.STRING)
+    private StoreDay closedDay;
 
     private String phone;
 
@@ -98,4 +101,13 @@ public class Store {
     @Enumerated(EnumType.STRING)
     private StoreDiscountInfo discountInfo;
 
+
+    // 해당 메서드는 실제 사용할 비즈니스 로직이 아닙니다. InitDb 에 사용할 메소드로 임시로 만들어둔것입니다.
+    public void updateCollection(StoreOperationHours storeOperationHours, StoreImages storeImages, Reviews reviews, Discounts discounts, Items items) {
+        this.items = items;
+        this.discounts = discounts;
+        this.storeOperationHours = storeOperationHours;
+        this.storeImages = storeImages;
+        this.reviews = reviews;
+    }
 }

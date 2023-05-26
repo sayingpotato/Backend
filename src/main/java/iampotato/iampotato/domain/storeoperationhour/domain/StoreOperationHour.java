9ed9d78f3009878745427ff2f200a67419d13efd
Jinhoon Park <g22203@naver.com>
2023-05-26 21:52:44 +0900

    feat : add storeOperationHour first class collection (#46)

---
+++ src/main/java/iampotato/iampotato/domain/storeoperationhour/domain/StoreOperationHour.java
@@ -0,0 +1,31 @@
+package iampotato.iampotato.domain.storeoperationhour.domain;
+
+import iampotato.iampotato.domain.store.domain.Store;
+import iampotato.iampotato.domain.store.domain.StoreDay;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StoreOperationHour {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "store_operation_hour_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @Enumerated(EnumType.STRING)
+    private StoreDay startDay;
+    @Enumerated(EnumType.STRING)
+    private StoreDay endDay;
+    private String startTime;
+    private String endTime;
+}

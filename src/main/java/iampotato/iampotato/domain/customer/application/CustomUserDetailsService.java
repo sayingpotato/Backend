43b2ac58a7074bdd1aaad904fe94030e1ad9c2d2
xylogan <xy.logandev@gmail.com>
2023-05-27 16:40:28 +0900

    feature : add a CustomUserDetailsService (#51)

---
+++ src/main/java/iampotato/iampotato/domain/customer/application/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package iampotato.iampotato.domain.customer.application;
+
+import lombok.RequiredArgsConstructor;
+import iampotato.iampotato.domain.customer.dao.CustomerRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final CustomerRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userRepository.findById(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저를 찾을 수 없습니다."));
+    }
+
+    private UserDetails createUserDetails(iampotato.iampotato.domain.customer.domain.Customer user) {
+//        System.out.println(user.getUsername());
+//        System.out.println(user.getPassword());
+//        System.out.println(user.getRoles());
+
+        return User.builder()
+                .username(user.getUsername())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .roles(user.getRoles().toArray(new String[0]))
+                .build();
+    }
+}

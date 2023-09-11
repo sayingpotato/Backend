0c4f9464e7d5beae75a66cd1d988455c411f4a06
Jinhoon Park <g22203@naver.com>
2023-09-11 21:05:50 +0900

    feat : add save repository

--- src/main/java/iampotato/iampotato/domain/order/dao/OrderRepository.java
+++ src/main/java/iampotato/iampotato/domain/order/dao/OrderRepository.java
@@ -13,6 +13,10 @@ public class OrderRepository {
 
     private final EntityManager em;
 
+    public void save(Order order) {
+        em.persist(order);
+    }
+
     public List<Order> findOrder(String userId) {
 
         return em.createQuery("select distinct o from Order o" +

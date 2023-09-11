56d323cfed30fa7e2a192c039a1904a924a7d5cf
Jinhoon Park <g22203@naver.com>
2023-09-11 21:19:24 +0900

    temp : fix cors

--- src/main/java/iampotato/iampotato/global/config/SecurityConfig.java
+++ src/main/java/iampotato/iampotato/global/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic().disable()
-                .cors().configurationSource(corsConfigurationSource())
+                .cors()
                 .and()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)

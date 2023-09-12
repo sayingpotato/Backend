318922ac94e075a6402eedcfbdac8416bbab4ddd
Jinhoon Park <g22203@naver.com>
2023-09-12 14:51:28 +0900

    fix : modify api spec

--- src/main/java/iampotato/iampotato/domain/store/dto/search/StoreSearchResponse.java
+++ src/main/java/iampotato/iampotato/domain/store/dto/search/StoreSearchResponse.java
@@ -6,9 +6,12 @@ import lombok.Data;
 @Data
 public class StoreSearchResponse {
 
+    private Long id;
+
     private String name;
 
     public StoreSearchResponse(Store store) {
+        this.id = store.getId();
         this.name = store.getName();
     }
 }

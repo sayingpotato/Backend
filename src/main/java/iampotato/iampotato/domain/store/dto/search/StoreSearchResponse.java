600c83f6f3c4113edb18a2cd975c7fac714827a6
Jinhoon Park <g22203@naver.com>
2023-10-04 16:30:32 +0900

    feat : modify search dto

--- src/main/java/iampotato/iampotato/domain/store/dto/search/StoreSearchResponse.java
+++ src/main/java/iampotato/iampotato/domain/store/dto/search/StoreSearchResponse.java
@@ -3,6 +3,8 @@ package iampotato.iampotato.domain.store.dto.search;
 import iampotato.iampotato.domain.store.domain.Store;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class StoreSearchResponse {
 
@@ -10,8 +12,17 @@ public class StoreSearchResponse {
 
     private String name;
 
-    public StoreSearchResponse(Store store) {
+    private String thumbnail;
+
+    boolean isFindByStore;
+
+    private List<String> itemNames;
+
+    public StoreSearchResponse(Store store, boolean isFindByStore, List<String> itemNames) {
         this.id = store.getId();
         this.name = store.getName();
+        this.thumbnail = store.getStoreTodayDiscountThumbnail();
+        this.isFindByStore = isFindByStore;
+        this.itemNames = itemNames;
     }
 }

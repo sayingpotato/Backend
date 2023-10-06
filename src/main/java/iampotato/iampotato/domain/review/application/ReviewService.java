a959f957c44f9a0c1df5d6f8a7ae80bf94c58856
Jinhoon Park <g22203@naver.com>
2023-10-06 17:32:50 +0900

    feat : add review content service

---
+++ src/main/java/iampotato/iampotato/domain/review/application/ReviewService.java
@@ -0,0 +1,30 @@
+package iampotato.iampotato.domain.review.application;
+
+import iampotato.iampotato.domain.review.dao.ReviewRepository;
+import iampotato.iampotato.domain.review.domain.Review;
+import iampotato.iampotato.domain.review.domain.ReviewDetail;
+import iampotato.iampotato.domain.review.dto.ReviewContentPostRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    @Transactional
+    public Review postReviewContent(ReviewContentPostRequest request) {
+
+        Review review = reviewRepository.findById(request.getReviewId());
+        review.reviewContent();
+
+        request.getReviewContents().stream()
+                .map(ReviewDetail::find)
+                .forEach(review::addReviewDetail);
+
+        return review;
+    }
+}

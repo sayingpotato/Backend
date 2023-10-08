49bc8c0efc274bb0bc23ea8a9604517063cd5031
Jinhoon Park <g22203@naver.com>
2023-10-08 18:47:48 +0900

    feat : add owner login token

--- src/main/java/iampotato/iampotato/domain/customer/dto/TokenResponse.java
+++ src/main/java/iampotato/iampotato/domain/customer/dto/TokenResponse.java
@@ -1,9 +1,11 @@
 package iampotato.iampotato.domain.customer.dto;
 
 import iampotato.iampotato.domain.customer.domain.CustomerStatus;
+import iampotato.iampotato.domain.owner.domain.OwnerStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+
 @Builder
 @Data
 @AllArgsConstructor
@@ -12,6 +14,8 @@ public class TokenResponse {
     private String accessToken;
 
     private CustomerStatus customerStatus;
+
+    private OwnerStatus ownerStatus;
 
     //    private String refreshToken;
 }

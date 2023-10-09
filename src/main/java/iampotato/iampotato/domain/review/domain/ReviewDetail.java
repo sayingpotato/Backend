9c6dbfa20b66d2c937e94f6b34c99fe3bbb3455e
Jinhoon Park <g22203@naver.com>
2023-10-09 12:17:07 +0900

    fix : add review content

--- src/main/java/iampotato/iampotato/domain/review/domain/ReviewDetail.java
+++ src/main/java/iampotato/iampotato/domain/review/domain/ReviewDetail.java
@@ -11,10 +11,18 @@ import java.util.stream.Stream;
 @Getter
 public enum ReviewDetail {
 
-    GREAT_COFFEE(1, "커피가 맛있어요"),
-    GREAT_BEVERAGE(1, "음료가 맛있어요"),
-    GREAT_FOOD(1, "음식이 맛있어요"),
-    MANY_OUTLET(1, "콘센트가 많아요");
+    GREAT_COFFEE(1, "☕ 커피가 맛있어요"),
+    GREAT_BEVERAGE(1, "\uD83C\uDF79 음료가 맛있어요"),
+    GREAT_FOOD(1, "\uD83E\uDD58 음식이 맛있어요"),
+    MANY_OUTLET(1, "\uD83D\uDD0C 콘센트가 많아요"),
+    GOOD_MOOD(1, "\uD83E\uDD29 분위기가 좋아요"),
+    GOOD_PRICE(1, "\uD83D\uDCB8 가성비가 좋아요"),
+    GOOD_WIFI(1, "\uD83D\uDCF6 와이파이가 잘 터져요"),
+    GOOD_DESERT(1, "\uD83E\uDDC1 디저트가 맛있어요"),
+    QUIET_MOOD(1, "\uD83E\uDD2B 조용해요"),
+    CLEAN_TOILET(1, "\uD83D\uDEBD 화장실이 깨끗해요"),
+    GOOD_SERVICE(1, "\uD83C\uDD93 서비스를 많이 줘요"),
+    GOOD_KIND(1, "\uD83D\uDE07 친절해요");
 
     private static final Map<String, ReviewDetail> REVIEWCONTENT_MAP =
             Collections.unmodifiableMap(Stream.of(values())

51c07809cbe85b334952dc0f49de5f541be82dd9
Jinhoon Park <g22203@naver.com>
2023-10-11 16:13:36 +0900

    feat : add get order request api

--- src/main/java/iampotato/iampotato/domain/order/api/OrderApi.java
+++ src/main/java/iampotato/iampotato/domain/order/api/OrderApi.java
@@ -2,10 +2,7 @@ package iampotato.iampotato.domain.order.api;
 
 import iampotato.iampotato.domain.order.application.OrderService;
 import iampotato.iampotato.domain.order.domain.Order;
-import iampotato.iampotato.domain.order.dto.OrderDetailResponse;
-import iampotato.iampotato.domain.order.dto.OrderDiscountsResponse;
-import iampotato.iampotato.domain.order.dto.OrderPostRequest;
-import iampotato.iampotato.domain.order.dto.OrderPostResponse;
+import iampotato.iampotato.domain.order.dto.*;
 import iampotato.iampotato.global.util.Result;
 import iampotato.iampotato.global.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,15 +20,31 @@ public class OrderApi {
     private final OrderService orderService;
 
     @Tag(name = "주문 페이지")
-    @Operation(summary = "주문 내역 요청", description = "현재 사용자의 주문내역을 반환합니다.")
+    @Operation(summary = "주문 내역 요청", description = "현재 Customer 의 주문내역을 반환합니다.")
     @GetMapping("api/v1/order/detail")
     public Result<List<OrderDetailResponse>> getOrderDetail(@RequestParam(value = "offset", defaultValue = "0") int offset,
-                                                            @RequestParam(value = "limit", defaultValue = "20") int limit) {
+                                                            @RequestParam(value = "limit", defaultValue = "100") int limit) {
 
         List<Order> orderDetails = orderService.getOrderDetail(SecurityUtil.getCurrentUserId(), offset, limit);
 
         List<OrderDetailResponse> responses = orderDetails.stream()
                 .map(OrderDetailResponse::new)
+                .collect(Collectors.toList());
+
+        return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);
+    }
+
+    @Tag(name = "점주")
+    @Operation(summary = "현재 주문 요청", description = "현재 Owner 가 받은 주문을 반환합니다.")
+    @GetMapping("api/v1/order/owner")
+    public Result<List<OrderOwnerResponse>> getOrderRequest(OrderOwnerRequest request,
+                                                            @RequestParam(value = "offset", defaultValue = "0") int offset,
+                                                            @RequestParam(value = "limit", defaultValue = "100") int limit) {
+
+        List<Order> orders = orderService.getOrderRequest(SecurityUtil.getCurrentUserId(), request, offset, limit);
+
+        List<OrderOwnerResponse> responses = orders.stream()
+                .map(OrderOwnerResponse::new)
                 .collect(Collectors.toList());
 
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);

10506c2794380626d0f51a1adba1bb74db5975fa
Jinhoon Park <g22203@naver.com>
2023-10-17 16:56:55 +0900

    feat : add daily profit statistics api

--- src/main/java/iampotato/iampotato/domain/order/api/OrderApi.java
+++ src/main/java/iampotato/iampotato/domain/order/api/OrderApi.java
@@ -115,7 +115,7 @@ public class OrderApi {
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);
     }
 
-    @Tag(name = "통계")
+    @Tag(name = "예측")
     @Operation(summary = "당일 아이템별 판매량 예측", description = "당일 상품별 판매량 예측 통계를 반환합니다.")
     @GetMapping("api/v1/statistics/daily/item")
     public Result<List<OrderDailyItemResponse>> getDailyItems(OrderDailyItemRequest request) {
@@ -125,7 +125,7 @@ public class OrderApi {
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);
     }
 
-    @Tag(name = "통계")
+    @Tag(name = "예측")
     @Operation(summary = "내일 아이템별 판매량 예측", description = "내일 상품별 판매량 예측 통계를 반환합니다.")
     @GetMapping("api/v1/statistics/daily/item/tomorrow")
     public Result<List<OrderDailyItemResponse>> getDailyItemsTomorrow(OrderDailyItemRequest request) {
@@ -135,7 +135,7 @@ public class OrderApi {
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);
     }
 
-    @Tag(name = "통계")
+    @Tag(name = "예측")
     @Operation(summary = "당일 수익/주문 예측", description = "당일 수익/주문에 대한 예측 통계를 반환합니다.")
     @GetMapping("api/v1/statistics/profit")
     public Result<OrderProfitResponse> getProfit(OrderDailyItemRequest request) {
@@ -145,7 +145,7 @@ public class OrderApi {
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, response);
     }
 
-    @Tag(name = "통계")
+    @Tag(name = "예측")
     @Operation(summary = "내일 수익/주문 예측", description = "내일 수익/주문에 대한 예측 통계를 반환합니다.")
     @GetMapping("api/v1/statistics/profit/tomorrow")
     public Result<OrderProfitResponse> getProfitTomorrow(OrderDailyItemRequest request) {
@@ -161,6 +161,16 @@ public class OrderApi {
     public Result<List<OrderMonthlyProfitResponse>> getMonthlyProfit(OrderMonthlyProfitRequest request) {
 
         List<OrderMonthlyProfitResponse> responses = orderService.findMonthlyProfit(SecurityUtil.getCurrentUserId(), request.getStoreId());
+
+        return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);
+    }
+
+    @Tag(name = "통계")
+    @Operation(summary = "일별 수익 통계", description = "일별 수익에 대한 통계를 반환합니다.")
+    @GetMapping("api/v1/statistics/daily/profit")
+    public Result<List<OrderDailyProfitResponse>> getDailyProfit(OrderDailyProfitRequest request) {
+
+        List<OrderDailyProfitResponse> responses = orderService.findDailyProfit(SecurityUtil.getCurrentUserId(), request.getStoreId());
 
         return new Result<>(Result.CODE_SUCCESS, Result.MESSAGE_OK, responses);
     }
